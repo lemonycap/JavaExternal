54a9d8178794aa8d1eae9c0be07c352e7ef1dcb7
yelyzaveta_onyshchenko <lemonycap@gmail.com>
2020-03-20 13:47:13 +0300

    parsing xml with dom added

--- Task6/src/main/java/com/example/parsers/Main.java
+++ Task6/src/main/java/com/example/parsers/Main.java
@@ -5,9 +5,16 @@ import com.example.parsers.utils.Helper;
 
 public class Main {
     public static void main (String [] args) {
+        System.out.println("Work with SAX parser:");
         SAXBuilder saxBuilder = new SAXBuilder();
         saxBuilder.buildSetCandies("src/main/resources/data/Candies.xml");
         System.out.println(saxBuilder.getCandies());
         Helper.printCandies(saxBuilder.getCandies());
+
+        System.out.println("Work with DOM parser:");
+        DOMBuilder domBuilder = new DOMBuilder();
+        domBuilder.buildSetCandies("src/main/resources/data/Candies.xml");
+        System.out.println(domBuilder.getCandies());
+        Helper.printCandies(domBuilder.getCandies());
     }
 }
